3ed734b75a42650b8a20df5362e199eabece2256
maidchauanh <trungnguyen123akohshit@gmail.com>
2019-12-12 16:25:58 +0700

    second commit

--- Level1State.java
+++ Level1State.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 public class Level1State extends GameState{
 
 	private TileMap tileMap;
@@ -10,14 +11,21 @@ public class Level1State extends GameState{
 	}
 
 	public void init(){
-		//tileMap = new TileMap();
+		tileMap = new TileMap(30);
+		tileMap.loadTiles("Tilesets/grasstileset.gif");
+		tileMap.loadMap("Maps/level1-1.map");
+		tileMap.setPosition(0,0);
 	}
 	public void update(){
 
 	}
 
 	public void draw(java.awt.Graphics2D g){
-
+		//clear screen 
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
+		//draw tilemap 
+		tileMap.draw(g);
 	}
 
 	public void keyPressed(int k){

5314751f812679d86a44244b4e0417451562af45
maidchauanh <trungnguyen123akohshit@gmail.com>
2019-12-12 16:15:57 +0700

    first commit

---
+++ Level1State.java
@@ -0,0 +1,33 @@
+public class Level1State extends GameState{
+
+	private TileMap tileMap;
+
+
+
+	public Level1State(GameStateManager gsm){
+		this.gsm = gsm;
+		init();
+	}
+
+	public void init(){
+		//tileMap = new TileMap();
+	}
+	public void update(){
+
+	}
+
+	public void draw(java.awt.Graphics2D g){
+
+	}
+
+	public void keyPressed(int k){
+
+	}
+	public void keyReleased(int k){
+
+	}
+
+
+
+
+}
